87aa480c551a6f31073701c43a41678c467c26d2
Ana <anapedrabcn@gmail.com>
2025-01-13 02:25:41 +0100

    final app

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "The Bubbly Baker",
+  description: "My amazing recipe app",
+};
+
+export default function RootLayout({ children }) {
+  let header = (
+    <header>
+      <Link href={"/"}>
+        <h1>The Bubbly Baker</h1>
+      </Link>
+    </header>
+  );
+
+  let footer = (
+    <footer>
+      <p>Made with 💛</p>
+    </footer>
+  );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {header}
+        {children}
+        {footer}
+      </body>
+    </html>
+  );
+}
